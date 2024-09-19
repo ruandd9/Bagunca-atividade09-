db852db6e4b4a9e475d7a48dacf3e9baf0b45899
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-09-19 20:38:47 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,52 @@
-// Obtém os modais
+//cria os modais
 var loginModal = document.getElementById("loginModal");
 var registerModal = document.getElementById("registerModal");
 
-// Obtém os links de Login e Cadastro
+//links de Login e Cadastro
 var loginLink = document.getElementById("loginLink");
 var registerLink = document.getElementById("registerLink");
 
-// Obtém os botões de fechar (X)
+//botões de fechar
 var closeLogin = document.getElementById("closeLogin");
 var closeRegister = document.getElementById("closeRegister");
 
-// Função para abrir o modal com animação
+//modal com animação
 function openModal(modal) {
     modal.style.display = "block";
     modal.classList.add('modal-slide-in');
 }
 
-// Função para fechar o modal e remover a animação
+//fechar o modal e remover a animação
 function closeModal(modal) {
     modal.classList.remove('modal-slide-in');
     setTimeout(function() {
         modal.style.display = "none";
-    }, 300); // Tempo para a animação de saída
+    }, 300); //timer da animação de saída
 }
 
-// Abre o modal de Login com animação
+//modal de Login com animação
 loginLink.addEventListener('click', function(event) {
     event.preventDefault();
     openModal(loginModal);
 });
 
-// Abre o modal de Cadastro com animação
+//modal de Cadastro com animação
 registerLink.addEventListener('click', function(event) {
     event.preventDefault();
     openModal(registerModal);
 });
 
-// Fecha o modal de Login
+// fecha o modal de login
 closeLogin.onclick = function() {
     closeModal(loginModal);
 }
 
-// Fecha o modal de Cadastro
+// fecha o modal de cadastro
 closeRegister.onclick = function() {
     closeModal(registerModal);
 }
 
-// Fecha os modais ao clicar fora
+// fecha os modais ao clicar fora
 window.onclick = function(event) {
     if (event.target == loginModal) {
         closeModal(loginModal);
